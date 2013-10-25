8596d1bb90cd6406ba6cab199e98412811ba55d5
TomV <tom@viner.tv>
2013-10-25 13:05:45 +0100

    add person form

--- lunchbox/core/views.py
+++ lunchbox/core/views.py
@@ -1,4 +1,5 @@
 from django.views.generic import CreateView
+from django.core.urlresolvers import reverse_lazy
 
 
 from .models import Person
@@ -7,6 +8,7 @@ from .models import Person
 class PersonView(CreateView):
     model = Person
     template_name = 'core/person_list.html'
+    success_url = reverse_lazy('core:home')
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
--- lunchbox/lunchbox/urls.py
+++ lunchbox/lunchbox/urls.py
@@ -5,8 +5,6 @@ admin.autodiscover()
 
 
 urlpatterns = patterns('',
-    # Examples:
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^', include('core.urls')),
-
+    url(r'^', include('core.urls', namespace='core')),
 )
