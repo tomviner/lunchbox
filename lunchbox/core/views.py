66e05cc92b157434dbbd3a31df0b36143061693d
TomV <tom@viner.tv>
2013-10-25 12:33:22 +0100

    connect up initial template

--- lunchbox/core/views.py
+++ lunchbox/core/views.py
@@ -8,6 +8,7 @@ from .models import Person
 
 class ListPerson(ListView):
     model = Person
+    template_name = 'resturant_picker/login.html'
 
 class CreatePerson(CreateView):
     model = Person
