33b18f04ba12721475e1c3cc3ddeded30b683547
Thomas Grainger <thomasgrainger@graingert.co.uk>
2013-10-25 12:34:41 +0100

    move person_list

--- lunchbox/core/views.py
+++ lunchbox/core/views.py
@@ -1,14 +1,12 @@
-from django.shortcuts import render
 from django.views.generic import CreateView, ListView
 
 
 from .models import Person
 
 
-
 class ListPerson(ListView):
     model = Person
-    template_name = 'resturant_picker/login.html'
+
 
 class CreatePerson(CreateView):
     model = Person
