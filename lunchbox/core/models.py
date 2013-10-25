3fda3c05df8e92044e8249ab2cf04eb7563034fa
Adriano Petrich <adrianopetrich@dtp881.hogarthww.prv>
2013-10-25 14:23:38 +0100

    include votes

--- lunchbox/core/models.py
+++ lunchbox/core/models.py
@@ -1,1 +1,10 @@
 from django.db import models
+
+
+class Restaurant(models.Model):
+    pass
+
+
+class Vote (models.Model):
+    date = models.DateField(auto_now_add=True)
+    people = models.ManyToManyField(Person)
